95871bf120e6eeeb78a11da098be0096fbeeb7c9
Digital <digital@c-23.local>
2019-01-08 11:58:11 +0100

    Add night mode

--- app/src/main/java/com/a73/hugo/duval/calculator/MainActivity.java
+++ app/src/main/java/com/a73/hugo/duval/calculator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.a73.hugo.duval.calculator;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 
 import com.a73.hugo.duval.calculator.calc.Calculator;
@@ -10,13 +14,36 @@ public class MainActivity extends AppCompatActivity {
 
     private Calculator calculator;
 
+    private boolean isNightModeEnabled = false;
+    private final String night_key = "NIGHT_KEY";
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.preferences =  PreferenceManager.getDefaultSharedPreferences(this);
+        this.isNightModeEnabled = preferences.getBoolean(night_key, false);
+
+        setTheme((this.isNightModeEnabled) ? R.style.WinterMode : R.style.SummerMode);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.calculator = new Calculator();
     }
 
+    /**
+     *
+     * @param v View
+     */
+    public void clickNightHandler(View v) {
+        SharedPreferences.Editor editor  = preferences.edit();
+        editor.putBoolean(night_key, !this.isNightModeEnabled);
+        editor.apply();
+
+        this.isNightModeEnabled = !this.isNightModeEnabled;
+
+        MainActivity.this.recreate();
+    }
     /**
      *
      * @param v View
